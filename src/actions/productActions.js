6d4ebd9a1c7fcc08cb81d35c46b03d4d88917887
conradevs <csilveiradev@gmail.com>
2022-10-24 00:00:20 -0300

    some other state actions for delete and edition

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,10 +1,17 @@
 import {
     ADD_PRODUCT,
     ADD_PRODUCT_SUCCESS,
-    ADD_PRODUCT_ERROR
+    ADD_PRODUCT_ERROR,
+    START_PRODUCTS_DOWNLOAD,
+    START_PRODUCTS_SUCCESS,
+    START_PRODUCTS_ERROR,
+    GET_PRODUCT_DELETE,
+    PRODUCT_DELETED_SUCCESS,
+    PRODUCT_DELETED_ERROR
 } from '../types';
 
 import axiosClient from '../config/axios';
+import Swal from 'sweetalert2';
 
 // Create new products
 export function createNewProductAction (product) {
@@ -15,10 +22,23 @@ export function createNewProductAction (product) {
             await axiosClient.post('/products',product);
             // if insert suceed, update state
             dispatch(addProductSucceed(product))
+            // Alert
+            Swal.fire(
+                'Done',
+                'Product added successfully',
+                'success'
+            )
         } catch (error) {
             console.log(error);
             // if theres an error change state
             dispatch(addProductError(true));
+            
+            // Error alert
+            Swal.fire({
+                icon: 'error',
+                title: 'Something went wrong',
+                text: 'There was an error, try again'
+            })
         }
     }
 }
@@ -38,4 +58,44 @@ const addProductSucceed = product => ({
 const addProductError = state => ({
     type: ADD_PRODUCT_ERROR,
     payload: state
+});
+
+// download products from database function
+export function getProductsAction() {
+    return async (dispatch) => {
+        dispatch(downloadProducts());
+        try {
+            const response = await axiosClient.get('./products');
+            dispatch(productsDowloaded(response.data));
+        } catch (error) {
+            dispatch(downloadProductsError())
+        }
+    }
+}
+
+const downloadProducts = () => ({
+    type: START_PRODUCTS_DOWNLOAD,
+    payload: true
+})
+
+const productsDowloaded = products => ({
+    type: START_PRODUCTS_SUCCESS,
+    payload:products
+})
+
+const downloadProductsError = () => ({
+    type: START_PRODUCTS_ERROR,
+    payload: true
+})
+
+// Select and delete product
+export function deleteProductAction(id) {
+    return async (dispatch) => {
+        dispatch(getProductDelete());
+    }
+}
+
+const getProductDelete = id => ({
+    type: GET_PRODUCT_DELETE,
+    payload: id
 })
